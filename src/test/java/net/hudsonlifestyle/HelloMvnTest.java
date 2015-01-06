5ca4f3d8b8687adf3bb8bb1f6c32f374cb678d5b
Glenn Castro <glenn.castro@gmail.com>
2015-01-06 18:32:13 +1100

    HudsonMvn - Adding JUnit

---
+++ src/test/java/net/hudsonlifestyle/HelloMvnTest.java
@@ -0,0 +1,13 @@
+package net.hudsonlifestyle;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloMvnTest {
+
+	@Test
+	public void testHelloMvn() throws Exception {
+		assertTrue(false);
+	}
+}
